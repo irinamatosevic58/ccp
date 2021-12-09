f876df7d198d39d53ce95c9c5a54646c447e153c
irinamatosevic58 <95843306+irinamatosevic58@users.noreply.github.com>
2021-12-09 08:40:46 +0100

    Add files via upload

--- Untitled2.c
+++ Untitled2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 void main()
 {
-int stranicaA;
-int stranicaB;
-int stranicaC;
+    int stranicaA;
+    int stranicaB;
+    int stranicaC;
     printf("unesi stranicu\n");
     scanf("%d %d %d", &stranicaA, &stranicaB, &stranicaC);
-int mnozenje = stranicaA * stranicaB * stranicaC;
-printf ("%d", mnozenje);
+    scanf("%d %d %d", &stranicaA, &stranicaB, &stranicaC);
+    int mnozenje = stranicaA * stranicaB * stranicaC;
+    printf ("%d", mnozenje);
 }
