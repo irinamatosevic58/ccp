b7c88afb724d0e5d57618e2768534dde0ba87225
irinamatosevic58 <95843306+irinamatosevic58@users.noreply.github.com>
2021-12-16 10:09:30 +0100

    Add files via upload

---
+++ Untitled28.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("4 > 6 -> %d.\n",(4>6));
+    printf("10 > 11 -> %d.\n", (10<11));
+    printf("5 >= 5 -> %d.\n", (5>=5));
+    printf("7 <= 14 -> %d.\n", (7<=14));
+    printf("9 != 9 -> %d.\n", (9!=9));
+    printf("10 == 10 -> %d.\n", (10==10));
+
+
+
+
+    return 0;
+}
