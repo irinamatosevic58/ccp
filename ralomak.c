fef82a1cdf3f9b4b1488faa879af3a9b844ac2f8
irinamatosevic58 <95843306+irinamatosevic58@users.noreply.github.com>
2021-12-23 08:43:57 +0100

    Add files via upload

--- ralomak.c
+++ ralomak.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 #include <math.h>
 
-int mainÿ()
+int main()
 {
 
     int nazivnik_1;
     int brojnik_1;
-    int nazivniki_2;
+    int nazivnik_2;
     int brojnik_2;
 
 
     printf("Unesi prvi brojnik i nazivnik \n");
-    scanf("%d %d", &brojnik_1, &nazinik_1);
+    scanf("%d %d", &brojnik_1, &nazivnik_1);
     printf("Unesi drugi brojnik i nazivnik \n");
-    scanf("%d %d, &brojnik_2, &nazivnik_2");
+    scanf("%d %d", &brojnik_2, &nazivnik_2);
     float racunanjePrvogRazlomka = (float) brojnik_1/nazivnik_1;
     float racunanjeDrugogRazlomka = (float) brojnik_2/ nazivnik_2;
-    float konacanRez = RcunajePrvogRazlomka + racunanjeDrugogRazlomka;
+    float konacanRez = racunanjePrvogRazlomka + racunanjeDrugogRazlomka;
 
-    int rez_brojnik=nazinik_1==nazinik_2?brojnik_1+bojnik_2:brojnik_1*nazivik_2+brojnik_2*nazivnik_1;
-    int rez_nazivnik=nazivni_1==nazivik_2?nazivi_1:nazivik_1+nazivnik_2;
-    printf("%d / %d\n", rez brojnik, rez_nazivnik);
+    int rez_brojnik=nazivnik_1==nazivnik_2?brojnik_1+brojnik_2:brojnik_1*nazivnik_2+brojnik_2*nazivnik_1;
+    int rez_nazivnik=nazivnik_1==nazivnik_2?nazivnik_1:nazivnik_1*nazivnik_2;
+    printf("%d / %d\n", rez_brojnik, rez_nazivnik);
     printf("%.2f",konacanRez);
 
     return 0;
