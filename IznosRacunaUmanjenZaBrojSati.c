bec7938d20ddc0b5ce77de82f7f87117131d2a3f
irinamatosevic58 <95843306+irinamatosevic58@users.noreply.github.com>
2022-01-13 10:39:07 +0100

    Add files via upload

---
+++ IznosRacunaUmanjenZaBrojSati.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ();
+{
+    int Cijena;
+    int Sati;
+    int Minute;
+
+    printf ("Unesite cijenu raèuna:\n");
+    scanf ("%d", %Cijena);
+    printf ("Unesite sate:\n");
+    scanf ("%d", %Sati);
+    printf ("Unesite minute:\n");
+    scanf ("%d", %Minute);
+
+
+
+
+
+}
