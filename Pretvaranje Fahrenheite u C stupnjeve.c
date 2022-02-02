739ce808a888780b6a90826ddf2e99b2e9c4bcb3
irinamatosevic58 <95843306+irinamatosevic58@users.noreply.github.com>
2022-02-02 20:30:42 +0100

    Update Pretvaranje Fahrenheite u C stupnjeve.c

--- Pretvaranje Fahrenheite u C stupnjeve.c
+++ Pretvaranje Fahrenheite u C stupnjeve.c
@@ -1,1 +1,19 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float F;
+    float C;
+    float K;
+
+    printf("Unesite Fahrenheite:\n");
+    scanf("%f", &F);
+
+   C=((float)F-32)*5/9;
+     K= C + 273.15;
+    printf("%.2f\n", C);
+    printf("%.2f\n", K);
+
+    return 0;
+}
