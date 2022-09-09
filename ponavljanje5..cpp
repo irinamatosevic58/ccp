c2f90ede1237deb71bb91be4dc65be4facfe0b45
irinamatosevic58 <95843306+irinamatosevic58@users.noreply.github.com>
2022-09-09 11:12:17 +0200

    Add files via upload

---
+++ ponavljanje5..cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+	
+   	int N;
+    int M;
+    int K;
+    int O;
+    int a;
+
+    printf("Koliko prijatelja zeli upisati tecaj:");
+    scanf("%d", &N);
+    printf("Koliki je maksimalan kapacitet:");
+    scanf("%d", &M);
+    printf("Koliko prijatelja se upisalo na tecaj:");
+    scanf("%d", &K);
+    
+  if(N<=M){
+        printf("Prijtelji se mogu upisati \n");
+      
+       
+
+  }
