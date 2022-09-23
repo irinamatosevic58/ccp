8c146b4f014200200f39b461ab56f5fe4fbfa2be
irinamatosevic58 <95843306+irinamatosevic58@users.noreply.github.com>
2022-09-23 11:20:31 +0200

    Add files via upload

---
+++ biljeznice.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int teke(float kg)
+{
+	int br = kg*1000/100;
+	return br;
+}
+int main()
+{
+	float kile;
+	int broj;
+	printf("unesi koliko kg pulpe:");
+	scanf("%f",& kile);
+	broj=teke(kile);
+	printf("od %.2f kg dobije se %d teki/biljeznica", kile, broj);
+	return 0;
+	
+}
